ad64b95fc7c2ed19bca798958dc55e062e01ce95
Abe Asami <sammy@nocono.net>
2018-05-25 08:10:43 +0900

    ひとまずH2に保存する形でAPI作成

---
+++ src/main/java/net/nocono/springbootdockersample/UserController.java
@@ -0,0 +1,37 @@
+package net.nocono.springbootdockersample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    List<User> show() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    void create(@ModelAttribute User user) {
+        userService.save(user);
+    }
+
+    @PostMapping("{id}")
+    void update(@PathVariable Long id, @ModelAttribute User user) {
+        userService.save(user);
+    }
+
+    @PostMapping("{id}/delete")
+    void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}

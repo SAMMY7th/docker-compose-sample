2f81c4702f851b02adcff8d54b8fb7d3c9263b76
Abe Asami <sammy@nocono.net>
2018-05-25 07:06:41 +0900

    Initial Commit

---
+++ src/main/java/net/nocono/springbootdockersample/SpringBootDockerSampleApplication.java
@@ -0,0 +1,12 @@
+package net.nocono.springbootdockersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerSampleApplication.class, args);
+	}
+}
